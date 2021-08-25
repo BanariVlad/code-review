829e92a02e1af421ce73c91328f0a3ae411bfad7
Denis Postolachi <deny.postolachi88@gmail.com>
2021-08-25 12:38:12 +0300

    task 10 add

---
+++ Задача №10 - функции обработки массивов/script/app.js
@@ -0,0 +1,10 @@
+let nums = [8, 7,  2,  2,  3, 4, 2,  5, 5, 6, 7, 8, 5, 9, 0, 1];
+
+const uniqueSorted = function deletesDuplicateFromArray(arr) {
+    let uniqueArray = arr.filter(function(item, pos) {
+        return arr.indexOf(item) == pos;
+    })
+    return uniqueArray.sort();
+}
+
+console.log(uniqueSorted(nums));
