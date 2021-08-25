5f2513be7267a9cacba6d370382c60fb02b66da8
Denis Postolachi <deny.postolachi88@gmail.com>
2021-08-25 11:47:22 +0300

    task 9 add

--- Задача №9 - функции обработки массивов/script/app.js
+++ Задача №9 - функции обработки массивов/script/app.js
@@ -1,4 +1,4 @@
-let arrayNotSorted = [1, 'a', 2, null, 3, undefined, 4, {}, 5, 6, `word`, 7, 8, false, 9, 100];
+let arrayNotSorted = [1, 'a', 2, null, 3, undefined, 4, {}, 5, 6, `wordd`, 7, 8, false, 9, 100];
 
 const filter = function arrayClear(arr) {
     let arrayNew = arr.filter(item => typeof item === "number");
